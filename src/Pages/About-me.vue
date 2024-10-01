4bef1980975f9f3feaa42393f8e66972ddff0f4c
tommy <tommyevertsen52@gmail.com>
2024-10-01 18:39:36 +0200

    test

--- src/Pages/About-me.vue
+++ src/Pages/About-me.vue
@@ -11,37 +11,48 @@
                 <p style="font-size: large">My name is Tommy Evertsen.</p>
 
                 <p class="pt-5" style="font-size: large">
-                  I am happily married and reside in Trondheim, where I enjoy a vibrant and fulfilling life.
+                  I am happily married and reside in Trondheim, where I enjoy a
+                  vibrant and fulfilling life.
                 </p>
 
                 <p class="pt-5" style="font-size: large">
-                  Outside of work, I have several hobbies that I am passionate about. I love traveling and exploring new
-                  places, as I believe that nothing broadens the mind like experiencing different cultures, cuisines,
-                  and landscapes. Traveling is a significant part of my life, offering me a sense of wonder and
-                  adventure. <br />
+                  Outside of work, I have several hobbies that I am passionate
+                  about. I love traveling and exploring new places, as I believe
+                  that nothing broadens the mind like experiencing different
+                  cultures, cuisines, and landscapes. Traveling is a significant
+                  part of my life, offering me a sense of wonder and adventure.
                   <br />
-                  I also find great joy in nature, whether it's hiking, cabin trips, or camping under the stars to the
-                  soothing sounds of birds and water. These experiences make me feel profoundly grateful and connected
-                  to the world around me.<br />
                   <br />
-                  In addition to my love for nature, I am deeply interested in continuous learning. I have a particular
-                  fascination with the universe, prehistoric Earth, and history, especially the Middle Ages and Viking
-                  era. Learning about these periods and their way of life is incredibly intriguing to me.
+                  I also find great joy in nature, whether it's hiking, cabin
+                  trips, or camping under the stars to the soothing sounds of
+                  birds and water. These experiences make me feel profoundly
+                  grateful and connected to the world around me.<br />
+                  <br />
+                  In addition to my love for nature, I am deeply interested in
+                  continuous learning. I have a particular fascination with the
+                  universe, prehistoric Earth, and history, especially the
+                  Middle Ages and Viking era. Learning about these periods and
+                  their way of life is incredibly intriguing to me.
                 </p>
                 <br />
 
                 <p class="pt-5" style="font-size: large">
-                  Lastly, I have a profound passion for programming and continuously seek to expand my knowledge in this
-                  field. The vast array of topics and technologies available for exploration is something I find
-                  immensely appealing. I particularly enjoy web development and video game development, both of which
-                  provide me with a great deal of satisfaction and enjoyment.
+                  Lastly, I have a profound passion for programming and
+                  continuously seek to expand my knowledge in this field. The
+                  vast array of topics and technologies available for
+                  exploration is something I find immensely appealing. I
+                  particularly enjoy web development and video game development,
+                  both of which provide me with a great deal of satisfaction and
+                  enjoyment.
                 </p>
 
                 <p class="pt-5" style="font-size: large">
                   Other hobbies include: fishing, video games, boardgames,
                   creating videogames in Unity, skiing, and
                   <span>
-                    <v-tooltip activator="parent" location="bottom">(Drinking beer)</v-tooltip>
+                    <v-tooltip activator="parent" location="bottom"
+                      >(Drinking beer)</v-tooltip
+                    >
                     more.
                   </span>
                 </p>
@@ -57,41 +68,99 @@
         </v-col>
         <v-col class="pa-2" cols="12" lg="6" md="12" sm="12" xs="12">
           <v-carousel v-if="smAndUp" height="600" cycle continuous touch>
-            <v-carousel-item src="@/assets/onthehand.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/onthehand.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/cabin.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/cabin.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/castle.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/castle.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/buffalo.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/buffalo.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/cactisforest.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/cactisforest.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/camping.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/camping.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/tree.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/tree.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/tekken.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/tekken.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/reefs.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/reefs.jpg"
+              cover
+            ></v-carousel-item>
           </v-carousel>
 
-          <v-carousel v-if="smAndDown" height="300" cycle continuous touch :show-arrows="false">
-            <v-carousel-item src="@/assets/images/cabin.jpg" cover></v-carousel-item>
+          <v-carousel
+            v-if="smAndDown"
+            height="300"
+            cycle
+            continuous
+            touch
+            :show-arrows="false"
+          >
+            <v-carousel-item
+              src="@/assets/images/cabin.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/castle.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/castle.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/buffalo.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/buffalo.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/cactisforest.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/cactisforest.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/camping.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/camping.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/tree.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/tree.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/tekken.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/tekken.jpg"
+              cover
+            ></v-carousel-item>
 
-            <v-carousel-item src="@/assets/images/reefs.jpg" cover></v-carousel-item>
+            <v-carousel-item
+              src="@/assets/images/reefs.jpg"
+              cover
+            ></v-carousel-item>
           </v-carousel>
         </v-col>
       </v-row>
@@ -117,7 +186,7 @@ const days = [
 const date = new Date();
 let day = days[date.getDay()];
 </script>
-
+<!-- Test -->
 <style>
 @media screen and (max-width: 768px) {
   .coding_journey {
@@ -137,5 +206,6 @@ let day = days[date.getDay()];
   color: #5b8fb9;
 }
 
-.day {}
+.day {
+}
 </style>
